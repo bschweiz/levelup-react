b0873b23c2cc3b6847259219ede6aa54b9f3640c
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-10 10:32:23 -0600

    added EventList to ApplicationViews, built out EventProvider as well. about to fire up npm server and test endpoints after testing server in postman first

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,17 +2,24 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
+import { EventList } from "./game/EventList.js"
+import { EventProvider } from "./game/EventProvider.js"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
-        }}> 
+        }}>
             <GameProvider>
-                <Route exact path="/">
-                    <GameList />
-                </Route>
+                <EventProvider>
+                    <Route exact path="/">
+                        <GameList />
+                    </Route>
+                    <Route exact path="/events">
+                        <EventList />
+                    </Route>
+                </EventProvider>
             </GameProvider>
         </main>
     </>
--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -7,15 +7,15 @@ export const GameList = (props) => {
     useEffect(() => {
         getGames()
     }, [])
-
+console.log(games);
     return (
         <article className="games">
             {
                 games.map(game => {
                     return <section key={`game--${game.id}`} className="game">
-                        <div className="game__title">{game.title} by {game.gamer}</div>
+                        <div className="game__title">{game.title} by {game.gamer.bio}</div>
                         <div className="game__players">{game.number_of_players} players needed</div>
-                        <div className="game__description">Short description: {game.description}</div>
+                        <div className="game__description">Game Type: {game.game_type.label}</div>
                     </section>
                 })
             }
