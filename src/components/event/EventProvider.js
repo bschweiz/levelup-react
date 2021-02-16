e1bf570fc20572dba7f66618f5a68645ffc8e1cc
Benjamin Schweizer <bschweiz@gmail.com>
2021-02-16 14:33:21 -0600

    added getEvents to last line of joinEvent function, leave events endpoint has been tested on the server side via postman, now just need to build out the ternary operator, the joined status state variable and the leave button

--- src/components/event/EventProvider.js
+++ src/components/event/EventProvider.js
@@ -27,6 +27,16 @@ export const EventProvider = (props) => {
             .then(getEvents)
     }
 
+    const leaveEvent = (eventId) => {
+        return fetch(`http://localhost:8000/events/${eventId}/signup`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
+        .then(getEvents)
+    }
+
     const joinEvent = (eventId) => {
         return fetch(`http://localhost:8000/events/${eventId}/signup`, {
             method: "POST",
@@ -35,6 +45,7 @@ export const EventProvider = (props) => {
             }
         })
             .then(response => response.json())
+            .then(getEvents)
     }
 
     return (
